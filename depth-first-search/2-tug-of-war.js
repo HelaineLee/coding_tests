3bb59f729a34fda154304965a3db57b28c3f15ff
HRLEE <puppleowl@etomato.com>
2025-02-17 15:29:16 +0900

    feat : add problem 6-2

---
+++ depth-first-search/2-tug-of-war.js
@@ -0,0 +1,46 @@
+const generateArray = (arr, enemy) => { // [1,2,3,4]
+  // console.log(arr);
+  if (arr.length === 0) return [];
+  if (arr.length === 1) return [arr.join('')];
+
+  const result = [];
+
+  for (let i=0 ; i<arr.length ; i++) {
+    const rest = [...arr.slice(0, i), ...arr.slice(i + 1)]; // Remove the current element
+    const perms = generateArray(rest, enemy); // Recursively get permutations of the rest
+    // console.log(rest, perms);
+
+    for (let perm of perms) {
+      let skip = false;
+      enemy.map(e => {
+        if(e.includes(parseInt(arr[i])) && e.includes(parseInt(perm[0]))) {
+          skip = true;
+        }
+      });
+      if(!skip){
+        result.push(arr[i] + perm); // Concatenate current element with each permutation
+      }
+    }
+  }
+
+  return result;
+}
+
+const getMinimumCases = (enemy, n) => {
+  let idx = 1;
+  const element = Array.from({length: n}, () => `${idx++}`);
+
+  const answer = generateArray(element, enemy);
+  // console.log(answer);
+
+  return answer.length;
+}
+
+console.log(getMinimumCases([[1, 3]], 4)); // => 12
+console.log(getMinimumCases([[1, 3], [2, 4]], 4)); // => 8
+console.log(getMinimumCases([[1, 3], [1, 4]], 4)); // => 4
+// console.log(getMinimumCases([[1, 7]], 7)); // => 3600
+// console.log(getMinimumCases([[1, 3], [5, 7], [4, 2]], 7)); // => 1968
+// console.log(getMinimumCases([[3, 2], [3, 5], [5, 2], [7, 3]], 7)); // => 864
+// console.log(getMinimumCases([[1, 2], [1, 5], [1, 7], [1, 3]], 7)); // => 720
+// console.log(getMinimumCases([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]], 7)); // => 646
