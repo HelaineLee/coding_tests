bab0d0bfaa42cdcb15aa52a8e938977f857cfdb5
HRLEE <puppleowl@etomato.com>
2025-03-16 12:34:32 +0900

    feat : add problem 7-1

---
+++ breadth-first-search/1-tile-jump.js
@@ -0,0 +1,46 @@
+// nums의 길이는 100을 넘지 않습니다.
+// 0 <= nums[i] // <= 10
+
+const getMinimumJump = (arr) => {
+	if(arr.length === 1){	// 도착
+		return 0;
+	}
+
+	if(arr[0] === 0){	// 진행 불가능
+		return -1;
+	}
+
+	let answer = -1;
+	for(let i=1 ; i<=arr[0] ; i++){
+		const nextArr = arr.slice(i);
+		let currentAnswer = 1;
+
+		if(nextArr.length > 0){
+			const jump = getMinimumJump(nextArr);
+			// console.log(arr, nextArr, jump);
+			if(jump > -1){
+				currentAnswer += jump;
+				if(currentAnswer > 0){
+					answer = answer === -1 ? currentAnswer : Math.min(answer, currentAnswer);
+				}
+			}
+		}
+
+	}
+
+	return answer;
+}
+
+// console.log(getMinimumJump([1, 2, 0, 1])); // 2
+// 1 2 0 0
+// console.log(getMinimumJump([2, 1, 1])); // 1
+// 2 0 0
+console.log(getMinimumJump([2, 2, 1, 2, 1, 1])); // 3
+// 2 0 1 2 0 0
+// 1 2 0 2 0 0
+console.log(getMinimumJump([1, 0, 1, 1, 3, 1, 2, 1])); // -1
+console.log(getMinimumJump([2, 3, 1, 0, 1, 1, 2, 3, 1, 5, 1, 3, 1])); // 7
+// 1 3 0 0 1 1 1 2 0 3 0 0 0 => 7
+// 1 3 0 0 1 1 2 0 1 3 0 0 0 => 7
+console.log(getMinimumJump([1, 2, 1, 2, 1, 2, 1, 1, 3, 1, 2, 1])); // 6
+console.log(getMinimumJump([1, 3, 2, 1, 1, 2, 3, 1, 3, 1, 2, 3, 5, 1, 5, 1, 2, 1, 1])); // 8
